43aeaf3b0f05b10d2e41a27a0a69ed6a9dcbbd0a
charleyangel <lydiargray@yahoo.com>
2017-07-09 17:55:36 +0100

    New project
    Dungeon Story

--- src/ctrl/initialize.js
+++
@@ -1,8 +0,0 @@
-// JavaScript source code
-/**
- * @fileOverview  Defining the main namespace ("shoppiong planner") and its MVC subnamespaces
- * @author Gerd Wagner
- */
-'use strict';
-// main namespace sp = "shopping planner"
-var sp = { model: {}, view: {}, ctrl: {} };
